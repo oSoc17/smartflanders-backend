a55f6f0794d94b7308c9a3a287fb7e1a3338eb51
Arne Gevaert <arne.gevaert@ugent.be>
2017-07-10 12:21:01 +0200

    first skeleton + update gitignore

---
+++ src/Datasets/Ixor/IxorSintNiklaas.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace oSoc\Smartflanders\Datasets\Ixor;
+use oSoc\Smartflanders\Helpers;
+
+class IxorSintNiklaas implements Helpers\IGraphProcessor
+{
+    public function getDynamicGraph()
+    {
+        // TODO: Implement getDynamicGraph() method.
+    }
+
+    public function getStaticGraph()
+    {
+        // TODO: Implement getStaticGraph() method.
+    }
+
+    public function getName()
+    {
+        // TODO: Implement getName() method.
+    }
+
+    public function getBaseUrl()
+    {
+        // TODO: Implement getBaseUrl() method.
+    }
+
+    public function getRealTimeMaxAge()
+    {
+        // TODO: Implement getRealTimeMaxAge() method.
+    }
+}
