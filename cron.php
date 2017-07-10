ee427544e1772c7db65fd992aac6c9c67a43acc6
Arne Gevaert <arne.gevaert@ugent.be>
2017-07-10 10:13:16 +0200

    restored cronjob

--- cron.php
+++ cron.php
@@ -27,26 +27,12 @@ if ($argc == 1) {
  * + triples for timestamp and filename of previous file
  */
 function acquire_data() {
-    /*$graph_processor_kortrijk = new ParkoKortrijk\ParkoToRDF();
+    $graph_processor_kortrijk = new ParkoKortrijk\ParkoToRDF();
     $graph_processor_ghent = new GentParking\GhentToRDF();
     $fsk = new Filesystem\FileWriter(__DIR__ . "/out", __DIR__ . "/resources", 300, $graph_processor_kortrijk);
     $fsg = new Filesystem\FileWriter(__DIR__ . "/out", __DIR__ . "/resources", 300, $graph_processor_ghent);
     $graph_k = $graph_processor_kortrijk->getDynamicGraph();
     $fsk->writeToFile(time(), $graph_k);
     $graph_g = $graph_processor_ghent->getDynamicGraph();
-    $fsg->writeToFile(time(), $graph_g);*/
-
-    $out_dirname = __DIR__ . "/out";
-    $res_dirname = __DIR__ . "/resources";
-    $second_interval = 300;
-    $graph_processor = new ParkoKortrijk\ParkoToRDF();
-    $fs = new Filesystem\FileSystemProcessor($out_dirname, $res_dirname ,$second_interval, $graph_processor);
-    $filename = $fs->getLastPage();
-    $fileReader = new Filesystem\FileReader($out_dirname, $res_dirname ,$second_interval, $graph_processor);
-    $graphs = $fileReader->getFullyDressedGraphsFromFile($filename);
-    $historic = true;
-    if ($filename === $fs->getLastPage()) {
-        $historic = false;
-    }
-    View::view('text/turtle', $graphs, $historic, $graph_processor->getBaseUrl());
+    $fsg->writeToFile(time(), $graph_g);
 }
