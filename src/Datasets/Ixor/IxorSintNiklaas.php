73e71110b4ad3bfb843948642b11aaf3183eabb4
Arne Gevaert <arne.gevaert@ugent.be>
2017-07-10 14:21:42 +0200

    added SintNiklaas Ixor data

--- src/Datasets/Ixor/IxorSintNiklaas.php
+++ src/Datasets/Ixor/IxorSintNiklaas.php
@@ -5,28 +5,56 @@ use oSoc\Smartflanders\Helpers;
 
 class IxorSintNiklaas implements Helpers\IGraphProcessor
 {
+    private $url = "https://smartflanders.ixortalk.com/api/v1.2/parkings/Sint-Niklaas";
+    private $base_url = "http://localhost:3000/dataset/Sint-Niklaas";
+
     public function getDynamicGraph()
     {
-        // TODO: Implement getDynamicGraph() method.
+        $time = time();
+        $graphname = $this->base_url . "?time=" . $time;
+        $graph = [
+            'prefixes' => Helpers\TripleHelper::getPrefixes(),
+            'triples' => []
+        ];
+
+        $authHeader = ['auth' =>
+            ['smartflanders', 'ySbwmmALC2z4cirWsEs8']
+        ];
+        $data = Helpers\RequestHelper::getJSON($this->url, $authHeader);
+        foreach($data->parkings as $parking) {
+            $subject = "http://open.data/stub/ixorstniklaas/" . str_replace(' ', '-', $parking->name);
+            $graph = Helpers\TripleHelper::addTriple($graph, $subject, 'datex:parkingNumberOfVacantSpaces', '"' . $parking->availableCapacity . '"');
+        }
+        return $graph;
     }
 
     public function getStaticGraph()
     {
-        // TODO: Implement getStaticGraph() method.
+        $graph = [
+            'prefixes' => Helpers\TripleHelper::getPrefixes(),
+            'triples' => []
+        ];
+        $data = Helpers\RequestHelper::getJSON($this->url, $authHeader);
+        foreach($data->parkings as $parking) {
+            $subject = "http://open.data/stub/ixorstniklaas/" . str_replace(' ', '-', $parking->name);
+            $graph = Helpers\TripleHelper::addTriple($graph, $subject, 'datex:parkingNumberOfSpaces', '"' . $parking->totalCapacity . '"');
+            $graph = Helpers\TripleHelper::addTriple($graph, $subject, 'geo:lat', '"' . $parking->latitude . '"');
+            $graph = Helpers\TripleHelper::addTriple($graph, $subject, 'geo:long', '"' . $parking->longitude . '"');
+        }
     }
 
     public function getName()
     {
-        // TODO: Implement getName() method.
+        return "SintNiklaas";
     }
 
     public function getBaseUrl()
     {
-        // TODO: Implement getBaseUrl() method.
+        return $this->base_url;
     }
 
     public function getRealTimeMaxAge()
     {
-        // TODO: Implement getRealTimeMaxAge() method.
+        return 30;
     }
 }
