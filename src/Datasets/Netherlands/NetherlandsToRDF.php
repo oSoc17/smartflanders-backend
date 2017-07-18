cfe57117ee641bbd1d5582cd710421a24fd769dc
Arne Gevaert <arnegevaert@gmail.com>
2017-07-18 16:29:24 +0200

    dynamic graph

--- src/Datasets/Netherlands/NetherlandsToRDF.php
+++ src/Datasets/Netherlands/NetherlandsToRDF.php
@@ -7,9 +7,47 @@ use Dotenv;
 class NetherlandsToRDF implements Helpers\IGraphProcessor
 {
 
+    private $publish_url, $fetch_url;
+
+    public function __construct()
+    {
+        $dotenv = new Dotenv\Dotenv(__DIR__ . '/../../../');
+        $dotenv->load();
+        $this->publish_url = $_ENV['NETHERLANDS_PUBLISH'];
+        $this->fetch_url = $_ENV['NETHERLANDS_FETCH'];
+    }
+
     public function getDynamicGraph()
     {
-        // TODO: Implement getDynamicGraph() method.
+        $time = time();
+        $graphname = $this->publish_url . "?time=" . date("Y-m-d\TH:i:s", $time);
+
+        $graph = [
+            'prefixes' => Helpers\TripleHelper::getPrefixes(),
+            'triples' => []
+        ];
+
+        $parkings = $this->getAccessibleParkings();
+        foreach($parkings as $parking) {
+            $subject = $this->publish_url . '#' . str_replace(' ', '-', $parking->name);
+            $vacant = $parking->status->vacantSpaces;
+            $graph = Helpers\TripleHelper::addTriple($graph, $subject, 'datex:parkingNumberOfVacantSpaces', '"' . $vacant . '"')
+        }
+
+        $multigraph = [
+            'prefixes' => $graph["prefixes"],
+            'triples' => []
+        ];
+
+        foreach ($graph["triples"] as $triple) {
+            $triple['graph'] = $graphname;
+            array_push($multigraph['triples'], $triple);
+        }
+
+        $gentime = '"' . date('c', $time) . '"^^http://www.w3.org/2001/XMLSchema#dateTime';
+        $multigraph = Helpers\TripleHelper::addTriple($multigraph, $graphname, "http://www.w3.org/ns/prov#generatedAtTime", $gentime);
+
+        return $multigraph;
     }
 
     public function getStaticGraph()
@@ -30,5 +68,19 @@ class NetherlandsToRDF implements Helpers\IGraphProcessor
     public function getRealTimeMaxAge()
     {
         // TODO: Implement getRealTimeMaxAge() method.
+    }
+
+    private function getAccessibleParkings() {
+        $jsondoc = Helpers\RequestHelper::getJSON($this->fetch_url);
+        $data = $jsondoc->ParkingFacilities;
+        $accessible_parkings = array();
+        foreach($data as $parking) {
+            if (array_key_exists('dynamicDataUrl', $parking)) {
+                if (!$parking->limitedAccess) {
+                    array_push($accessible_parkings, $parking);
+                }
+            }
+        }
+        return $accessible_parkings;
     }
 }
