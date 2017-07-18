d9af862759353546244bfaf1023896ac89671135
Arne Gevaert <arnegevaert@gmail.com>
2017-07-18 16:06:40 +0200

    scaffold

---
+++ src/Datasets/Netherlands/NetherlandsToRDF.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace oSoc\Smartflanders\Datasets\Netherlands;
+use oSoc\Smartflanders\Helpers;
+use Dotenv;
+
+class NetherlandsToRDF implements Helpers\IGraphProcessor
+{
+
+    public function getDynamicGraph()
+    {
+        // TODO: Implement getDynamicGraph() method.
+    }
+
+    public function getStaticGraph()
+    {
+        // TODO: Implement getStaticGraph() method.
+    }
+
+    public function getName()
+    {
+        // TODO: Implement getName() method.
+    }
+
+    public function getBaseUrl()
+    {
+        // TODO: Implement getBaseUrl() method.
+    }
+
+    public function getRealTimeMaxAge()
+    {
+        // TODO: Implement getRealTimeMaxAge() method.
+    }
+}
