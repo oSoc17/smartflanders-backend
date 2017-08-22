002ca78f5464f942fd0414cc8ac5062b33364020
root <root@w135h082.wireless.ugent.be>
2017-08-22 14:05:24 +0200

    hotfix gent to have Linked as name

--- src/Datasets/GentParking/GhentToRDF.php
+++ src/Datasets/GentParking/GhentToRDF.php
@@ -98,7 +98,7 @@ Class GhentToRDF implements Helpers\IGraphProcessor
 
     public function getName()
     {
-        return "Gent";
+        return "Linked";
     }
 
     public function getBaseUrl()
