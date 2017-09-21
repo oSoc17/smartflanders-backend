e6db044276e04d8e4605618c6e92e0d731fa539b
Arne Gevaert <arnegevaert@gmail.com>
2017-09-21 13:57:07 +0200

    even more refactor

--- public/index.php
+++ public/index.php
@@ -12,31 +12,8 @@ use Dotenv\Dotenv;
 $dotenv = new Dotenv(__DIR__ . '/../');
 $dotenv->load();
 
-$datasets_gather = explode(',', $_ENV["DATASETS_GATHER"]);
-$processors_gather = array();
-foreach($datasets_gather as $dataset) {
-    try {
-        $dotenv->required($dataset . "_PATH");
-        $class = $_ENV[$dataset . "_PATH"];
-        array_push($processors_gather, new $class);
-    } catch (\Exception $e) {
-        error_log("Invalid .env configuration: dataset " . $dataset . " has no corresponding class path."
-            . " Please add the variable " . $dataset . "_PATH.");
-    }
-}
-
-$datasets = explode(',', $_ENV["DATASETS"]);
-$processors = array();
-foreach($datasets as $dataset) {
-    try {
-        $dotenv->required($dataset . "_PATH");
-        $class = $_ENV[$dataset . "_PATH"];
-        array_push($processors, new $class);
-    } catch (\Exception $e) {
-        error_log("Invalid .env configuration: dataset " . $dataset . " has no corresponding class path."
-            . " Please add the variable " . $dataset . "_PATH.");
-    }
-}
+$processors = get_datasets_from_names(explode(',', $_ENV['DATASETS']), $dotenv);
+$processors_gather = get_datasets_from_names(explode(',', $_ENV["DATASETS_GATHER"]), $dotenv);
 
 $nameToGP = [];
 foreach($processors as $gp) {
@@ -52,3 +29,18 @@ $second_interval = 300;
 $router = new Router($_SERVER['HTTP_HOST'], $out_dirname, $res_dirname, $second_interval, $nameToGP, $processors_gather);
 $router->init();
 $router->run();
+
+function get_datasets_from_names($names, $dotenv) {
+    $result = array();
+    foreach($names as $dataset) {
+        try {
+            $dotenv->required($dataset . "_PATH");
+            $class = $_ENV[$dataset . "_PATH"];
+            array_push($result, new $class);
+        } catch (\Exception $e) {
+            error_log("Invalid .env configuration: dataset " . $dataset . " has no corresponding class path."
+                . " Please add the variable " . $dataset . "_PATH.");
+        }
+    }
+    return $result;
+}
