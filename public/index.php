501caa6fecd95793ddecb89c37b702d4866683ff
Arne Gevaert <arnegevaert@gmail.com>
2017-09-21 13:46:54 +0200

    more refactor

--- public/index.php
+++ public/index.php
@@ -1,16 +1,17 @@
 <?php
 
+// Vocab for statistical data: https://www.w3.org/TR/vocab-data-cube/
+
 namespace oSoc\Smartflanders;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 use oSoc\Smartflanders\Datasets;
-use Bramus\Router;
-use oSoc\Smartflanders\Helpers\RangeGateIntervalCalculator;
 use Dotenv\Dotenv;
 
 $dotenv = new Dotenv(__DIR__ . '/../');
 $dotenv->load();
+
 $datasets_gather = explode(',', $_ENV["DATASETS_GATHER"]);
 $processors_gather = array();
 foreach($datasets_gather as $dataset) {
@@ -19,7 +20,7 @@ foreach($datasets_gather as $dataset) {
         $class = $_ENV[$dataset . "_PATH"];
         array_push($processors_gather, new $class);
     } catch (\Exception $e) {
-        error_log("Invalid .env configuration: dataset " . $dataset . " was has no corresponding class path."
+        error_log("Invalid .env configuration: dataset " . $dataset . " has no corresponding class path."
             . " Please add the variable " . $dataset . "_PATH.");
     }
 }
@@ -32,7 +33,7 @@ foreach($datasets as $dataset) {
         $class = $_ENV[$dataset . "_PATH"];
         array_push($processors, new $class);
     } catch (\Exception $e) {
-        error_log("Invalid .env configuration: dataset " . $dataset . " was has no corresponding class path."
+        error_log("Invalid .env configuration: dataset " . $dataset . " has no corresponding class path."
             . " Please add the variable " . $dataset . "_PATH.");
     }
 }
@@ -48,77 +49,6 @@ $out_dirname = __DIR__ . "/../out";
 $res_dirname = __DIR__ . "/../resources";
 $second_interval = 300;
 
-// Reverse proxy: this code routes between different datasets
-// This is only necessary because multiple datasets are being hosted on the same domain.
-$router = new Router\Router();
-$router->set404(function() {echo "Page not found.";});
-
-// Initialize range gate calculator and initialize dataset class
-$found = false;
-$dataset_name = explode('.', $_SERVER['HTTP_HOST'])[0];
-$dataset = null; $fs = null; $calc = null;
-foreach($nameToGP as $name => $gp) {
-    if ($name === $dataset_name) {
-        $found = true;
-        $dataset = $nameToGP[$dataset_name];
-        $fs = new Filesystem\FileSystemProcessor($out_dirname, $res_dirname ,$second_interval, $dataset);
-        $calc = new RangeGateIntervalCalculator($_ENV['RANGE_GATES_CONFIG'], $fs->getOldestTimestamp());
-    }
-}
-
-$router->get('/parking', function(){
-    global $found, $dataset, $out_dirname, $res_dirname, $second_interval, $processors_gather;
-    if ($found) {
-        //dataset($dataset);
-        View::view($dataset, $out_dirname, $res_dirname, $second_interval, $processors_gather);
-    } else {
-        http_response_code(404);
-        die("Route not found: " . $dataset);
-    }
-});
-
-$router->get('/parking/rangegate', function() {
-    global $found; global $dataset; global $calc; global $fs;
-    echo "This is root range gate.<br>";
-    if ($found) {
-        echo "Dataset: " . $dataset->getName() . "<br>";
-        $subgates = $calc->getRootSubRangeGates($fs->getOldestTimestamp());
-        echo "subgates: <br>";
-        foreach ($subgates as $gate) {
-            $start = date('Y-m-d\TH:i:s',$gate[0]);
-            $end = date('Y-m-d\TH:i:s',$gate[1]);
-            echo $start . "_" . $end . "<br>";
-        }
-    } else {
-        echo "Dataset not found.<br>";
-    }
-
-});
-
-$router->get('/parking/rangegate/([^/]+)', function($gatename) {
-    global $found; global $dataset; global $calc;
-    echo "Sub range gate " . $gatename . ".<br>";
-    if ($found) {
-        echo "Dataset: " . $dataset->getName() . ".<br>";
-        if ($calc->isLegal($gatename)) {
-            echo "Range gate name is legal.<br>";
-            $subgates = $calc->getSubRangeGates($gatename);
-            if ($subgates) {
-                echo "subgates: <br>";
-                foreach ($subgates as $gate) {
-                    $start = date('Y-m-d\TH:i:s', $gate[0]);
-                    $end = date('Y-m-d\TH:i:s',$gate[1]);
-                    echo $start . "_" . $end . "<br>";
-                }
-            } else {
-                echo "Sublevel is leaf level.<br>";
-            }
-        } else {
-            echo "Illegal range gate name.<br>";
-        }
-    } else {
-        echo "Dataset not found.<br>";
-    }
-});
-
+$router = new Router($_SERVER['HTTP_HOST'], $out_dirname, $res_dirname, $second_interval, $nameToGP, $processors_gather);
+$router->init();
 $router->run();
---
+++ src/Router.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace oSoc\Smartflanders;
+
+use Bramus;
+use Dotenv\Dotenv;
+use oSoc\Smartflanders\Helpers\RangeGateIntervalCalculator;
+
+class Router
+{
+    private $router;
+    private $http_host;
+    private $out_dirname;
+    private $res_dirname;
+    private $second_interval;
+    private $nameToGP;
+    private $processors_gather;
+
+    public function __construct($http_host, $out_dirname, $res_dirname, $second_interval, $nameToGP, $processors_gather) {
+        $this->router = new Bramus\Router\Router();
+        $this->router->set404(function() {echo "Page not found.";});
+
+        $this->http_host = $http_host;
+        $this->out_dirname = $out_dirname;
+        $this->res_dirname = $res_dirname;
+        $this->second_interval = $second_interval;
+        $this->nameToGP = $nameToGP;
+        $this->processors_gather = $processors_gather;
+
+        $dotenv = new Dotenv(__DIR__ . '/../');
+        $dotenv->load();
+    }
+
+    public function init() {
+        $out_dirname = $this->out_dirname;
+        $res_dirname = $this->res_dirname;
+        $second_interval = $this->second_interval;
+        $processors_gather = $this->processors_gather;
+
+        $found = false;
+        $dataset_name = explode('.', $this->http_host)[0];
+        $dataset = null; $fs = null; $calc = null;
+        foreach($this->nameToGP as $name => $gp) {
+            if ($name === $dataset_name) {
+                $found = true;
+                $dataset = $this->nameToGP[$dataset_name];
+                $fs = new Filesystem\FileSystemProcessor($out_dirname, $res_dirname ,$second_interval, $dataset);
+                $calc = new RangeGateIntervalCalculator($_ENV['RANGE_GATES_CONFIG'], $fs->getOldestTimestamp());
+            }
+        }
+
+        $this->router->get('/parking',
+            function() use ($found, $dataset, $out_dirname, $res_dirname, $second_interval, $processors_gather) {
+                if ($found) {
+                    View::view($dataset, $out_dirname, $res_dirname, $second_interval, $processors_gather);
+                } else {
+                    http_response_code(404);
+                    die("Route not found: " . $dataset);
+                }
+            }
+        );
+
+        $this->router->get('/parking/rangegate',
+            function() use ($found, $dataset, $fs, $calc) {
+                echo "This is root range gate.<br>";
+                if ($found) {
+                    echo "Dataset: " . $dataset->getName() . "<br>";
+                    $subgates = $calc->getRootSubRangeGates($fs->getOldestTimestamp());
+                    echo "subgates: <br>";
+                    foreach ($subgates as $gate) {
+                        $start = date('Y-m-d\TH:i:s',$gate[0]);
+                        $end = date('Y-m-d\TH:i:s',$gate[1]);
+                        echo $start . "_" . $end . "<br>";
+                    }
+                } else {
+                    echo "Dataset not found.<br>";
+                }
+            }
+        );
+
+        $this->router->get('/parking/rangegate/([^/]+)',
+            function($gatename) use ($found, $dataset, $calc){
+                //global $found; global $dataset; global $calc;
+                echo "Sub range gate " . $gatename . ".<br>";
+                if ($found) {
+                    echo "Dataset: " . $dataset->getName() . ".<br>";
+                    if ($calc->isLegal($gatename)) {
+                        echo "Range gate name is legal.<br>";
+                        $subgates = $calc->getSubRangeGates($gatename);
+                        if ($subgates) {
+                            echo "subgates: <br>";
+                            foreach ($subgates as $gate) {
+                                $start = date('Y-m-d\TH:i:s', $gate[0]);
+                                $end = date('Y-m-d\TH:i:s',$gate[1]);
+                                echo $start . "_" . $end . "<br>";
+                            }
+                        } else {
+                            echo "Sublevel is leaf level.<br>";
+                        }
+                    } else {
+                        echo "Illegal range gate name.<br>";
+                    }
+                } else {
+                    echo "Dataset not found.<br>";
+                }
+            }
+        );
+    }
+
+    public function run() {
+        $this->router->run();
+    }
+}
