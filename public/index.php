c5d92712810c86d3c831ed5c4afcb8093eafaef1
Arne Gevaert <arnegevaert@gmail.com>
2017-11-02 22:12:34 +0100

    removed unnecessary code

--- public/index.php
+++ public/index.php
@@ -9,10 +9,6 @@ namespace oSoc\Smartflanders;
 require __DIR__ . '/../vendor/autoload.php';
 
 use oSoc\Smartflanders\Datasets;
-use Dotenv\Dotenv;
-
-$dotenv = new Dotenv(__DIR__ . '/../');
-$dotenv->load();
 
 $settings = new Settings();
 $processors = $settings->getDatasets();
@@ -20,9 +16,6 @@ $processors_gather = $settings->getDatasetsGather();
 $out_dirname = $settings->getOutDir();
 $res_dirname = $settings->getResourcesDir();
 $second_interval = $settings->getDefaultGatherInterval();
-
-//$processors = get_datasets_from_names(explode(',', $_ENV['DATASETS']), $dotenv);
-//$processors_gather = get_datasets_from_names(explode(',', $_ENV["DATASETS_GATHER"]), $dotenv);
 
 $nameToGP = [];
 foreach($processors as $gp) {
@@ -33,19 +26,3 @@ foreach($processors as $gp) {
 $router = new Router($out_dirname, $res_dirname, $second_interval, $nameToGP, $processors_gather);
 $router->init();
 $router->run();
-
-/*
-function get_datasets_from_names($names, $dotenv) {
-    $result = array();
-    foreach($names as $dataset) {
-        try {
-            $dotenv->required($dataset . "_PATH");
-            $class = $_ENV[$dataset . "_PATH"];
-            array_push($result, new $class);
-        } catch (\Exception $e) {
-            error_log("Invalid .env configuration: dataset " . $dataset . " has no corresponding class path."
-                . " Please add the variable " . $dataset . "_PATH.");
-        }
-    }
-    return $result;
-}*/
